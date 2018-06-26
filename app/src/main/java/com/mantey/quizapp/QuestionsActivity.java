b333886a6d3ede677cd12b619fa966da4dc179ad
mantey <contactmantey@gmail.com>
2018-06-26 20:13:05 +0100

    Initail commit of QuizApp

---
+++ app/src/main/java/com/mantey/quizapp/QuestionsActivity.java
@@ -0,0 +1,18 @@
+package com.mantey.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+
+    public void submitButton(View view) {
+
+    }
+}
